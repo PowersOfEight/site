335282db669f02783b6281f876200a5dd6a08d79
James Daniel Johnson <jdjohnson8883@gmail.com>
2022-05-02 20:49:18 -0400

    Focused the code on the creation of the canvas with an id for reference

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-console.log("Hello World");
 
+
+/**
+ * appends a new canvas to the body once the DOM content
+ * is loaded.
+ */
 document.addEventListener("DOMContentLoaded", ()=>{
-    if (document.body !== null) {
-        let canvas = document.createElement("canvas");
-        canvas.setAttribute("height", "350");
-        canvas.setAttribute("width", "750");
-        document.body.appendChild(canvas);
-        let ctx = canvas.getContext("2d");
-        console.log("success...");
-    }
-    else {
-        console.warn("can not load body");
-    }
+
+    const canvas = document.createElement("canvas");
+    
+    canvas.setAttribute("id", "siteCanvas");
+    document.body.appendChild(canvas);
+    console.log("Canvas \"siteCanvas\" successfully loaded");
+
 });
